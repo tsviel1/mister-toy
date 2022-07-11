1bcfdad759569dce7981bce472dfb2e6b3bc3441
Tsviel Sillam <tsviel15@gmail.com>
2022-07-11 17:23:31 +0300

    finished cleaning

---
+++ src/views/about.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>about mister toy...</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'about'
+
+}
+</script>
+
+<style>
+</style>
---
+++ src/views/home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>home sweet home</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'home'
+
+}
+</script>
+
+<style>
+</style>
